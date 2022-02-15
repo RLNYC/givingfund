4929202568ec55c4394f00165826eb9319ec5300
You Song Hou <yousonghou@gmail.com>
2022-02-14 19:57:41 -0500

    clean up

--- contracts/GivingFund.sol
+++ contracts/GivingFund.sol
@@ -14,34 +14,12 @@ contract GivingFund is ERC721 {
     uint public prizePoolWon = 0;
     uint public charityAmount = 0;
     address payable _owner;
-    mapping(uint => StakeInfo) public stakelist;
 
     constructor(TicketToken _stakeToken, TicketToken _ticketToken) ERC721("Giving Fund NFT", "GFNFT") public {
         _owner = msg.sender;
         stakeToken = _stakeToken;
         ticketToken = _ticketToken;
     }
-
-    struct StakeInfo {
-        uint nftid;
-        uint startDate;
-        uint stakeAmount;
-        address payable from;
-    }
-
-    event Staked (
-        uint nftid,
-        uint startDate,
-        uint stakeAmount,
-        address payable from
-    );
-
-    event Unstaked (
-        uint nftid,
-        uint startDate,
-        uint stakeAmount,
-        address payable from
-    );
 
     event WonWheel (
         address buyer,
@@ -50,54 +28,6 @@ contract GivingFund is ERC721 {
         uint randomNumber,
         uint wheelNumber
     );
-
-    // Stake AVAX for Stake Token and get NFT
-    function stakeforTokens() payable public  {
-        prizePool +=  msg.value * 7 / 100;
-        charityAmount += msg.value * 3 / 100;
-        totalDonation += msg.value;
-        stakeToken.mint(msg.sender, msg.value);
-        
-        // Create NFT
-        uint _tokenId = totalSupply().add(1);
-        _safeMint(msg.sender, _tokenId);
-
-        // Record user stake infor
-        stakelist[_tokenId] = StakeInfo(_tokenId, block.timestamp, msg.value, msg.sender);
-
-        emit Staked(_tokenId, block.timestamp, msg.value, msg.sender);
-    }
-
-    // Unstake Stake Token and get back AVAX
-    // Transfer the NFT owned by sender to contract address
-    function unstakeToken(uint _nftid) payable public  {
-        StakeInfo storage userData = stakelist[_nftid];
-
-        require(ownerOf(_nftid) == msg.sender, "You do not own this NFT");
-
-        // Send the NFT owned by sender to contract address
-        transferFrom(msg.sender, address(this), _nftid);
-
-        // Remove Stake Tokens from the sender and send back AVAX
-        stakeToken.burn(msg.sender, userData.stakeAmount);
-        msg.sender.transfer(userData.stakeAmount);
-        totalDonation -= userData.stakeAmount;
-
-        emit Unstaked(_nftid, block.timestamp, msg.value, msg.sender);
-    }
-
-    // Claim Ticket Tokens if the user hold Stake Token for a week
-    function claimTicketTokens(uint _nftid) public {
-        StakeInfo storage userData = stakelist[_nftid];
-
-        // 605543 = 1 week
-        require(block.timestamp - userData.startDate > 605543, "You need to wait at least a week to claim Ticket Tokens");
-        require(ownerOf(_nftid) == msg.sender, "You do not own this NFT");
-        ticketToken.mint(msg.sender, userData.stakeAmount * 2);
-
-        // New start date
-        userData.startDate = block.timestamp;
-    }
 
     // Pay 1 Ticket token to spin the wheel and a chance to earn reward
     function useTicketToken() public {
