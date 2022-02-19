50bedc67aa59cf76cb881c9ad35ad1a5e2e712e0
You Song Hou <yousonghou@gmail.com>
2022-02-18 19:02:41 -0500

    display NFTs in Gift Matching Donation page

--- src/components/mygiving/GiftMatchingDonation.js
+++ src/components/mygiving/GiftMatchingDonation.js
@@ -1,15 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
-import { Card, InputNumber, Button, Typography } from 'antd';
+import { Row, Col, Card, InputNumber, Button, Typography } from 'antd';
 
 function GiftMatchingDonation({ walletAddress, ethProvider, givingFundBlockchain }) {
   const [ethBalance, setETHBalance] = useState(0);
   const [depositAmount, setDepositAmount] = useState(0);
   const [depositLoading, setDepositLoading] = useState(false);
+  const [nfts, setNFTs] = useState([]);
+  const [sentAmount, setSentAmount] = useState(0);
 
   useEffect(() => {
     if(walletAddress) getBalance();
   }, [walletAddress]);
+
+  useEffect(() => {
+    if(givingFundBlockchain) getNFTs();
+  }, [givingFundBlockchain]);
+
+  const getNFTs = async () => {
+    const totalSupply = await givingFundBlockchain.totalSupply();
+    let oldnfts = [];
+
+    for(let i = 1; i <= +totalSupply; i++){
+      const tokenOwner = await givingFundBlockchain.ownerOf(i);
+      
+      if(tokenOwner === walletAddress){
+        let data = await givingFundBlockchain.giftList(i);
+        console.log(data);
+        oldnfts.push(data);
+        setSentAmount(sentAmount + +data.amount.toString());
+      }
+    }
+    console.log(oldnfts);
+    setNFTs(oldnfts);
+  }
 
   const getBalance = async () => {
     const balance = await ethProvider.getBalance(walletAddress);
@@ -39,10 +63,17 @@ function GiftMatchingDonation({ walletAddress, ethProvider, givingFundBlockchain
     }
   }
 
+  const getDate = (dateTimeStamp) => {
+    const date = new Date(dateTimeStamp * 1000); // x1000 to convert from seconds to milliseconds 
+    let stringDate = date.toUTCString();
+    stringDate = stringDate.substring(0, stringDate.indexOf("GMT")) + "UTC";
+    return stringDate;
+  }
+
   return (
     <div>
       <Typography.Title level={3}>
-        Your sent matching gift:  1 aETH
+        Your sent matching gift:  {sentAmount / 10 ** 18} aETH
       </Typography.Title>
       <p>All unused matching donation gift will expired in in a year and refunded to your account</p>
       <Typography.Title level={3}>
@@ -75,6 +106,21 @@ function GiftMatchingDonation({ walletAddress, ethProvider, givingFundBlockchain
           Submit
         </Button>
       </Card>
+      <br />
+      <Typography.Title level={3}>
+        Your Matching NFTs
+      </Typography.Title>
+      <Row gutter={16}>
+        {nfts.map(nft => (
+          <Col className="gutter-col" sm={{ span: 24 }} md={{ span: 8 }} key={nft.nftid.toString()}>
+            <Card>
+              <h2>NFT Id: {nft.nftid.toString()}</h2>
+              <p>Stake Amount: {nft.amount.toString() / 10 ** 18} AVAX</p>
+              <p>Start Date: {getDate(nft.startDate.toString())}</p>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   )
 }
